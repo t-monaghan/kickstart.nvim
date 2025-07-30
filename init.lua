344119eb076d0768798bbb4fc585a5c64fbf5bfe
t-monaghan <tom.monaghan@cultureamp.com>
2025-07-30 14:17:13 +1000

    adds gopls and taplo

--- init.lua
+++ init.lua
@@ -663,8 +663,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         nil_ls = {},
+        taplo = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
