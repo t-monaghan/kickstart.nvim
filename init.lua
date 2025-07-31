516f04c628bfb3707e12b0e03908b2c50521b81e
t-monaghan <tom.monaghan@cultureamp.com>
2025-07-31 13:46:09 +1000

    turns softwrapping off

--- init.lua
+++ init.lua
@@ -98,6 +98,8 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.o.wrap = false
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
