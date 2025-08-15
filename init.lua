0e4b768635f156c625041732f999355bbe798809
t-monaghan <tom.monaghan@cultureamp.com>
2025-08-15 13:20:39 +1000

    formats quotes on treesitter context

--- init.lua
+++ init.lua
@@ -858,7 +858,7 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-  {"nvim-treesitter/nvim-treesitter-context"},
+  { 'nvim-treesitter/nvim-treesitter-context' },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
