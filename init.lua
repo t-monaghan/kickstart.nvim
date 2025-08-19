f23f6b99427efc310caab84d128d2818f2b78acd
t-monaghan <tom.monaghan@cultureamp.com>
2025-08-19 15:52:21 +1000

    adds git blame

--- init.lua
+++ init.lua
@@ -265,6 +265,10 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 0,
+      },
       signs = {
         add = { text = '+' },
         change = { text = '~' },
